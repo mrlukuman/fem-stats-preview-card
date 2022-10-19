25954be0bcc831a5dc89f67d270e6c064d2fc3f5
Tajudeen <74155780+tjalani@users.noreply.github.com>
2022-10-19 19:41:18 +0300

    Finalized

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import mobileImage from "./assets/image-header-mobile.jpg";
+import "./index.css";
 
 function App() {
   return (
-    <div className="">
-      <img alt="yo" src={mobileImage}></img>
-      <div>
-        <h1>
-          Get <span>insights</span> that help your business grow.
-        </h1>
-        <p>
-          Discover the benefits of data analytics and make better decisions
-          regarding revenue, customer experience, and overall efficiency.
-        </p>
-      </div>
-      <div>
-        <div>
-          <h1>10K+</h1>
-          <p>COMPANIES</p>
-        </div>
-        <div>
-          <h1>314</h1>
-          <p>TEMPALTES</p>
-        </div>
-        <div>
-          <h1>12M+</h1>
-          <p>QUERIES</p>
+    <div className="main text-center leading-loose">
+      <div className=" flex flex-col justify-center w-96 rounded-lg bg-card-bg text-white m-12 md:flex-row-reverse md:w-3/4">
+        <img
+          alt="hero"
+          className="rounded-t-lg md:rounded-t-none md:w-full"
+          src={mobileImage}
+        ></img>
+
+        <div className="flex justify-center flex-col px-6 py-6">
+          <div>
+            <h1 className="pb-6 text-3xl">
+              Get <span className="text-violet">insights</span> that help your
+              business grow.
+            </h1>
+            <p>
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+          <div className="py-6 space-y-4 md:flex md:flex-row md:content-center md:justify-center md:space-x-6 md:space-y-0 md:pt-10">
+            <div>
+              <h1 className="text-2xl">10K+</h1>
+              <p className="text-xs opacity-50">COMPANIES</p>
+            </div>
+            <div>
+              <h1 className="text-2xl">314</h1>
+              <p className="text-xs opacity-50">TEMPLATES</p>
+            </div>
+            <div>
+              <h1 className="text-2xl">12M+</h1>
+              <p className="text-xs opacity-50">QUERIES</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "card-bg": "hsl(244, 38%, 16%)",
+      white: "#FFF",
+      violet: "hsl(277, 64%, 61%)",
+    },
+    extend: {
+      backgroundImage: {
+        hero_mobile: "url(../src/assets/image-header-mobile.jpg)",
+      },
+    },
   },
   plugins: [],
 };
